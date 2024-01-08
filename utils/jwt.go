67406648a77f3743dbacf95ebd1d4ef430f5b920
AnggaPutraa <yananggaa@gmail.com>
2024-01-08 22:59:13 +0800

    feat(utils): initialized jwt strategy

---
+++ utils/jwt.go
@@ -0,0 +1,117 @@
+package utils
+
+import (
+	"errors"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+const (
+	AccessTokenType  = "ACCESS"
+	RefreshTokenType = "REFRESH"
+)
+
+var (
+	ErrExpiredToken = errors.New("Token has expired")
+	ErrInvalidToken = errors.New("Token is invalid")
+)
+
+type JWTClaims struct {
+	Sub       string    `json:"sub"`
+	Email     string    `json:"email"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+func (c *JWTClaims) Valid() error {
+	if time.Now().After(c.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
+
+type Strategy interface {
+	GenerateToken(userId, email string) (string, string, error)
+	VerifyToken(token, tokenType string) (*JWTClaims, error)
+}
+
+type JWTStrategy struct {
+	accessTokenSecret  string
+	refreshTokenSecret string
+}
+
+func NewJWTStrategy(accessTokenSecret, refreshTokenSecret string) Strategy {
+	return &JWTStrategy{
+		accessTokenSecret:  accessTokenSecret,
+		refreshTokenSecret: refreshTokenSecret,
+	}
+}
+
+func (strategy *JWTStrategy) GenerateToken(userId, email string) (string, string, error) {
+	accessTokenClaims := jwt.MapClaims{
+		"sub":        userId,
+		"email":      email,
+		"issued_at":  time.Now(),
+		"expired_at": time.Now().Add(15 * time.Minute).Unix(),
+	}
+
+	refreshTokenClaims := jwt.MapClaims{
+		"sub":        userId,
+		"email":      email,
+		"issued_at":  time.Now(),
+		"expired_at": time.Now().Add(7 * 24 * time.Hour).Unix(),
+	}
+
+	accessToken, err := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		accessTokenClaims,
+	).SignedString([]byte(strategy.accessTokenSecret))
+	if err != nil {
+		return "", "", err
+	}
+
+	refreshToken, err := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		refreshTokenClaims,
+	).SignedString([]byte(strategy.refreshTokenSecret))
+	if err != nil {
+		return "", "", err
+	}
+
+	return accessToken, refreshToken, nil
+}
+
+func (strategy *JWTStrategy) VerifyToken(token, tokenType string) (*JWTClaims, error) {
+	tokenSecret := strategy.accessTokenSecret
+	if tokenType == RefreshTokenType {
+		tokenSecret = strategy.refreshTokenSecret
+	}
+	keyFunc := func(jwtToken *jwt.Token) (interface{}, error) {
+		_, ok := jwtToken.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+
+		return []byte(tokenSecret), nil
+	}
+
+	accessToken, err := jwt.ParseWithClaims(token, &JWTClaims{}, keyFunc)
+
+	if err != nil {
+		vErr, ok := err.(*jwt.ValidationError)
+		if ok && errors.Is(vErr.Inner, ErrInvalidToken) {
+			return nil, ErrInvalidToken
+		}
+		return nil, ErrInvalidToken
+	}
+
+	payload, ok := accessToken.Claims.(*JWTClaims)
+
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+
+	return payload, nil
+}
